c6a4c1bfc224c29ca13164e10a9f73063a687475
homin <ghals5737@gmail.com>
2022-06-02 22:56:06 +0900

    User Controller 추가

--- restapi/src/main/kotlin/com/quiz/restapi/user/controller/UserController.kt
+++ restapi/src/main/kotlin/com/quiz/restapi/user/controller/UserController.kt
@@ -1,5 +1,8 @@
 package com.quiz.restapi.user.controller
 
+import com.quiz.restapi.user.entity.User
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestAttribute
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -7,9 +10,9 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/users")
 class UserController {
 
-    @GetMapping
-    fun getUser(Long userId):User{
-        return null;
+    @GetMapping()
+    fun getUser(@RequestAttribute userId:Long): User {
+        return User(userId,"aa");
     }
 
 }
